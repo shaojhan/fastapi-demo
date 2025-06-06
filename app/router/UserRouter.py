eedacdd8c3cf847b9780da4c8921bdca14d2e356
shaojhan <jhanzihsian@gmail.com>
2025-06-06 11:23:29 +0800

    add_user_query_service

--- app/router/UserRouter.py
+++ app/router/UserRouter.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from app.router.schemas.UserSchema import UserCreate, UserRead
 from app.repositories.UserRepository import UserRepository
 from app.services.UnitOfWork import UserUnitOfWork
-from app.services.UserService import UserService
+from app.services.UserService import UserService, UserQueryService
 
 from app.exceptions.UserException import UserHasAlreadyExistedError
 
@@ -10,6 +10,9 @@ router = APIRouter(prefix='/users', tags=['user'])
 
 def get_user_service() -> UserService:
     return UserService()
+
+def get_user_query_service() -> UserQueryService:
+    return UserQueryService()
 
 @router.post('/create', operation_id='create_user')
 async def create_user(
@@ -19,7 +22,11 @@ async def create_user(
         user = await user_service.add_user_or_fail(request_body)
         return user
 
-
+@router.get('/get_all', operation_id='get_all_users')
+async def get_all_users(
+    user_query_service: UserQueryService = Depends(get_user_query_service)
+):
+    return await user_query_service.get_all_user()
 
 @router.post('/profile/create', operation_id='create_user_profile')
 async def create_user_profile(request_body):
