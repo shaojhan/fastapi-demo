986caeecddfac24c9eda75e2660e83527abbf524
jhan <jhanzihsian@gmail.com>
2026-01-23 10:16:29 +0800

    remove_workflow_domain

--- tests/unit/domain/test_workflow_domain.py
+++ tests/unit/domain/test_workflow_domain.py
@@ -1,40 +1,40 @@
-from SpiffWorkflow.specs.WorkflowSpec import WorkflowSpec
-from SpiffWorkflow.specs.ExclusiveChoice import ExclusiveChoice
-from SpiffWorkflow.specs.Cancel import Cancel
-from SpiffWorkflow.specs.Simple import Simple
-from SpiffWorkflow.specs.base import TaskSpec
-from SpiffWorkflow.specs.StartTask import StartTask
+# from SpiffWorkflow.specs.WorkflowSpec import WorkflowSpec
+# from SpiffWorkflow.specs.ExclusiveChoice import ExclusiveChoice
+# from SpiffWorkflow.specs.Cancel import Cancel
+# from SpiffWorkflow.specs.Simple import Simple
+# from SpiffWorkflow.specs.base import TaskSpec
+# from SpiffWorkflow.specs.StartTask import StartTask
 
-from SpiffWorkflow.operators import Equal, Attrib
+# from SpiffWorkflow.operators import Equal, Attrib
 
 
-def my_nuclear_strike(msg, *args, **kwargs):
-    print("Launched: ", msg)
+# def my_nuclear_strike(msg, *args, **kwargs):
+#     print("Launched: ", msg)
 
-# Define workflow spec
-class NuclearStrikeWorkflowSpec(WorkflowSpec):
-    def __init__(self):
-        super().__init__()
-        self.start = StartTask(wf_spec=self)
+# # Define workflow spec
+# class NuclearStrikeWorkflowSpec(WorkflowSpec):
+#     def __init__(self):
+#         super().__init__()
+#         self.start = StartTask(wf_spec=self)
 
-        general_choice = ExclusiveChoice(wf_spec=self, name='general')
-        self.start.connect(task_spec=general_choice)
+#         general_choice = ExclusiveChoice(wf_spec=self, name='general')
+#         self.start.connect(task_spec=general_choice)
 
-        cancel = Cancel(wf_spec=self, name='workflow_aborted')
-        general_choice.connect(taskspec=cancel)
+#         cancel = Cancel(wf_spec=self, name='workflow_aborted')
+#         general_choice.connect(taskspec=cancel)
 
-        president_choice = ExclusiveChoice(wf_spec=self, name='president')
-        cond = Equal(Attrib(name='confirmation'), 'yes')
+#         president_choice = ExclusiveChoice(wf_spec=self, name='president')
+#         cond = Equal(Attrib(name='confirmation'), 'yes')
 
-        strike = Simple(wf_spec=self, name='nuclear_strike')
-        president_choice.connect_if(condition=cond, task_spec=strike)
+#         strike = Simple(wf_spec=self, name='nuclear_strike')
+#         president_choice.connect_if(condition=cond, task_spec=strike)
 
-        strike.completed_event.connect(callback=my_nuclear_strike)
+#         strike.completed_event.connect(callback=my_nuclear_strike)
         
-from SpiffWorkflow.workflow import Workflow
-from SpiffWorkflow.task import Task, TaskStates
+# from SpiffWorkflow.workflow import Workflow
+# from SpiffWorkflow.task import Task, TaskStates
 
-workflow_instance = Workflow(workflow_spec=NuclearStrikeWorkflowSpec())
-workflow_instance.dump()
+# workflow_instance = Workflow(workflow_spec=NuclearStrikeWorkflowSpec())
+# workflow_instance.dump()
 
-workflow_instance.get_tasks(first_task='Start')
+# workflow_instance.get_tasks(first_task='Start')
